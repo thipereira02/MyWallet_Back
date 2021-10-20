fb7ae7ceb2b414193d7f3bd361034797756a589e
thipereira02 <thiago.spereira02@gmail.com>
2021-10-20 15:14:19 -0300

    Setting server

---
+++ src/server.js
@@ -0,0 +1,8 @@
+import "./setup.js"; 
+import app from "./app.js";
+
+const port = Number(process.env.PORT);
+
+app.listen(port, () => {
+	console.log(`Server running on port ${process.env.PORT}`);
+});
