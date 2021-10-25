a210a8cffe1db14ea421e4e5f075dfdf65a58780
thipereira02 <thiago.spereira02@gmail.com>
2021-10-25 14:40:18 -0300

    Adding tests to signin

--- tests/users.test.js
+++ tests/users.test.js
@@ -14,7 +14,7 @@ afterAll(() => {
 const agent = supertest(app);
 
 describe ("POST /signup", () => {
-    it ("should return status 201 when data is ok", async () => {
+    it ("should returns status 201 when data is ok", async () => {
         const body = {
             name: "Test",
 			email: "test@email.com",
@@ -26,7 +26,7 @@ describe ("POST /signup", () => {
         expect(result.status).toEqual(201);
     });
 
-    it ("should return status 400 when body has any data as invalid", async () => {
+    it ("should returns status 400 when body has any data as invalid", async () => {
         const body = {
             name: "Test",
 			email: "test",
@@ -38,14 +38,14 @@ describe ("POST /signup", () => {
         expect(result.status).toEqual(400);
     });
 
-    it ("should return status 400 when body is empty", async () => {
+    it ("should returns status 400 when body is empty", async () => {
         const body = {}
 
         const result = await agent.post("/signup").send(body);
         expect(result.status).toEqual(400);
     });
 
-    it ("should return status 409 when email is already registered", async () => {
+    it ("should returns status 409 when email is already registered", async () => {
         const body = {
             name: "Test",
 			email: "test@email.com",
@@ -56,5 +56,57 @@ describe ("POST /signup", () => {
         await agent.post("/signup").send(body);
         const result = await agent.post("/signup").send(body);
         expect(result.status).toEqual(409);
+    });
+});
+
+describe ("POST /signin", () => {
+    beforeEach(async () => {
+        const body = {
+          name: "Test",
+          email: "test@email.com",
+          password: "123456",
+          confirmPassword: "123456"
+        };
+        await agent.post("/signup").send(body);
+    });
+
+    it ("should returns status 400 when signIn data is invalid", async () => {
+        const body = {
+            email: "test@email.com",
+			password: "12"
+        }
+
+        const result = await agent.post("/signin").send(body);
+        expect(result.status).toEqual(400);
+    });
+
+    it ("should returns status 401 when email doesn't exists", async () => {
+        const body = {
+            email: "test@ema.com",
+			password: "123456"
+        }
+
+        const result = await agent.post("/signin").send(body);
+        expect(result.status).toEqual(401);
+    });
+
+    it ("should returns status 401 when password is incorrect", async () => {
+        const body = {
+            email: "test@email.com",
+			password: "1234567"
+        }
+
+        const result = await agent.post("/signin").send(body);
+        expect(result.status).toEqual(401);
+    });
+
+    it ("should returns status 200 when email and password are ok", async () => {
+        const body = {
+            email: "test@email.com",
+			password: "123456"
+        }
+
+        const result = await agent.post("/signin").send(body);
+        expect(result.status).toEqual(200);
     });
 });
